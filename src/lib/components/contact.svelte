32654371146a4b0bb506b3f09638952be16ff1f7
Rahul1342002 <trahul13402@gmail.com>
2024-04-08 21:54:47 +0530

    added conditions for forms

--- src/lib/components/contact.svelte
+++ src/lib/components/contact.svelte
@@ -33,6 +33,28 @@
         </p>
           </span>
         </div>
+        <!-- <div class="flex space-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5 text-cyan-300"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
+              clip-rule="evenodd"
+            />
+          </svg>
+          <span>
+
+            <p class="font-bold underline">Venture Address</p>
+
+        <p class="section-text">
+          Ravada,Bhogapuram Vizianagaram(Dt.) Andhrapradesh.
+        </p>
+          </span>
+        </div> -->
         <div class="flex space-x-2">
           <svg
             xmlns="http://www.w3.org/2000/svg"
