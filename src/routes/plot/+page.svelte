e685808b5744a0c12c4f66306f92fa8d1f1c7e03
Rahul1342002 <trahul13402@gmail.com>
2024-04-09 14:52:24 +0530

    Plot Brochure added

--- src/routes/plot/+page.svelte
+++ src/routes/plot/+page.svelte
@@ -9,5 +9,13 @@
     <div class="">
         <Navbar/>
         <Plot/>
+        <div class="w-full">
+  <a
+    href="/brochure.pdf"
+    class="fixed bottom-96 cursor-pointer -right-20 -rotate-90 bg-[#0B294F] text-white px-4 py-4 rounded-xl"
+  >
+    <span class="font-bold">Download Brochure</span>
+  </a>
+</div>
     </div>
 </section>
