1abf7bb8cc9b8051172d8179bf3d247a512812f9
Rahul1342002 <trahul13402@gmail.com>
2024-04-10 10:38:42 +0530

    about leadership

--- src/routes/aboutus/Components/aboutus.svelte
+++ src/routes/aboutus/Components/aboutus.svelte
@@ -1,45 +1,3 @@
-
-<div id="contact" class="flex w-full min-h-screen justify-center items-center p-2">
-  <div
-    class="flex flex-col-reverse overflow-hidden md:flex-row md:space-x-6 md:space-y-0 space-y-6 bg-[#0B294F] w-full max-w-4xl p-8 sm:p-12 rounded-xl shadow-lg text-white"
-  >
-    <div class="flex flex-col   space-y-8">
-      <div>
-        <h1 class="font-bold text-2xl tracking-wide underline text-center mt-4 md:text-left">About Us</h1>
-        <p class="pt-2 text-cyan-100 text-sm md:text-base text-justify">
-          Welcome to BVR New Way Ventures, your trusted partner in the world of real estate. We specialize in offering prime plots of land that promise not just a place to build, but a canvas for your dreams to take shape. Discover a new way to invest and build your future with us. 
-        </p>
-      </div>
-      <div class="flex flex-col space-y-4">
-        
-        <div class="flex space-x-2">
-          
-          <span>
-
-            <p class="font-bold underline text-2xl text-center md:text-left">Our Mission</p>
-
-            <p class="text-sm md:text-base text-cyan-100 pt-2 leading-relaxed  text-justify ">
-              Our mission is to empower individuals and families to achieve their real estate goals by offering high-quality plots of land in desirable locations. We believe that owning land is not just a financial investment, but a cornerstone of wealth creation and legacy building.
-            </p>
-            
-          </span>
-        </div>
-        
-      </div>
-      
-    </div>
-    <div class="relative">
-      
-      <div class="bg-white rounded-xl shadow-lg p-8 text-gray-600 md:w-80 z-10">
-        <img class="rounded-2xl" src="/login.png" alt="Login Image" />
-      </div>
-    </div>
-  </div>
-</div>
-
-
-
-
 <section class="about" id="about">
   <div class="container">
     <figure class="about-banner">
@@ -110,6 +68,81 @@
     </div>
   </div>
 </section>
+
+<div id="contact" class="flex w-full min-h-screen justify-center items-center p-2">
+  <div
+    class="flex flex-col-reverse overflow-hidden md:flex-row md:space-x-6 md:space-y-0 space-y-6 bg-[#0B294F] w-full max-w-4xl p-8 sm:p-12 rounded-xl shadow-lg text-white"
+  >
+    <div class="flex flex-col   space-y-8">
+      <div>
+        <h1 class="font-bold text-2xl tracking-wide underline text-center mt-4 md:text-left">About Us</h1>
+        <p class="pt-2 text-cyan-100 text-sm md:text-base text-justify">
+          Welcome to BVR New Way Ventures, your trusted partner in the world of real estate. We specialize in offering prime plots of land that promise not just a place to build, but a canvas for your dreams to take shape. Discover a new way to invest and build your future with us. 
+        </p>
+      </div>
+      <div class="flex flex-col space-y-4">
+        
+        <div class="flex space-x-2">
+          
+          <span>
+
+            <p class="font-bold underline text-2xl text-center md:text-left">Our Mission</p>
+
+            <p class="text-sm md:text-base text-cyan-100 pt-2 leading-relaxed  text-justify ">
+              Our mission is to empower individuals and families to achieve their real estate goals by offering high-quality plots of land in desirable locations. We believe that owning land is not just a financial investment, but a cornerstone of wealth creation and legacy building.
+            </p>
+            
+          </span>
+        </div>
+        
+      </div>
+      
+    </div>
+    <div class="relative">
+      
+      <div class="bg-white rounded-xl shadow-lg p-8 text-gray-600 md:w-80 z-10">
+        <img class="rounded-2xl" src="/login.png" alt="Login Image" />
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="contact" class="flex w-full min-h-screen justify-center items-center p-2">
+  <div
+    class="flex flex-col-reverse overflow-hidden md:flex-row md:space-x-6 md:space-y-0 space-y-6  w-full max-w-4xl p-8 sm:p-12 rounded-xl  text-[#0B294F]"
+  >
+  <div class="relative">
+      
+      <div class="bg-white rounded-xl shadow-lg p-8 text-gray-600 md:w-80 z-10">
+        <img class="rounded-2xl" src="/login.png" alt="Login Image" />
+      </div>
+    </div>
+    <div class="flex flex-col   ">
+      <p class=" text-fs-5 font-light py-1 px-5 bg-blue-300 inline-block rounded-full mb-4 mr-auto text-[#0B294F] ">Leadership</p>
+
+      <h2 class="text-xl  font-bold">
+        Discover Our Story: Pioneering Excellence in Real Estate Development.
+      </h2>
+
+      <p class="about-text text-justify">
+        Elevating lifestyles through visionary real estate solutions, we
+        redefine luxury with innovation and integrity. Experience excellence in
+        every detail with our distinguished portfolio of properties.
+      </p>
+
+      
+
+      <p class="callout-1">
+        "Crafting Exceptional Living Experiences, Enriching Lifestyles, and Redefining Luxury Living Standards."
+      </p>
+      
+
+      
+    </div>
+  </div>
+</div>
+
+
 
 
 
