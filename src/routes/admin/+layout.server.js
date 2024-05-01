810bed087e676883e4b3cbdb727fce0bae66643a
SandeepDev1 <106228847+SandeepDev1@users.noreply.github.com>
2024-05-01 23:56:01 +0530

    .

--- src/routes/admin/+layout.server.js
+++ src/routes/admin/+layout.server.js
@@ -4,18 +4,13 @@ import {getLogger} from "$lib/logs/index.js";
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ cookies }) {
-    try {
-        const token = cookies.get('access_token');
-        if(!token || token.length === 0) {
-            return redirect(301, "/login")
-        }
-        // const result = verifyJwt(token)
-        // await getLogger().debug(JSON.stringify(result))
-        // if(result === false) {
-        //     return redirect(301, "/login")
-        // }
-    } catch(err) {
-        await getLogger().log(JSON.stringify(err))
+    const token = cookies.get('access_token');
+    if(!token || token.length === 0) {
+        return redirect(301, "/login")
+    }
+    const result = verifyJwt(token)
+    await getLogger().debug(JSON.stringify(result))
+    if(result === false) {
         return redirect(301, "/login")
     }
 
