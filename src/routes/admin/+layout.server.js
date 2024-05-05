ec882d71da8ae441068a80c7cb21dd24b4588e89
SandeepDev1 <106228847+SandeepDev1@users.noreply.github.com>
2024-05-05 22:52:05 +0530

    update plot data API fix

--- src/routes/admin/+layout.server.js
+++ src/routes/admin/+layout.server.js
@@ -1,9 +1,13 @@
 import {verifyJwt} from "$lib/jwt/auth.js";
 import {redirect} from "@sveltejs/kit";
-import {getLogger} from "$lib/logs/index.js";
+import {browser} from "$app/environment";
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ cookies }) {
+    if(!browser) {
+        return
+    }
+
     const token = cookies.get('access_token');
     if (!token || token.length === 0) {
         return redirect(302, "/login")
