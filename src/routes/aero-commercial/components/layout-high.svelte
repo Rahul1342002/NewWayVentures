a71f2f09216df14b44ca00e9258b3cb18dc57d70
Chaitanya <Chaitanyadwarapudi123@gmail.com>
2024-05-17 21:28:06 +0530

    done with the changes in reponsive design

--- src/routes/aero-commercial/components/layout-high.svelte
+++ src/routes/aero-commercial/components/layout-high.svelte
@@ -17,7 +17,7 @@
           </div>
         </div>
         <h3
-            class="text-[#0B294F] mt-5 text-center text-2xl font-bold hover:text-[#02adee] cursor-pointer"
+            class="text-[#0B294F] mt-5 text-center md:text-2xl font-bold hover:text-[#02adee] cursor-pointer"
           >
             Beautiful Entry Arch
           </h3>
@@ -39,13 +39,13 @@
           </div>
         </div>
         <h3
-            class="text-[#0B294F] mt-5 text-center text-2xl font-bold hover:text-[#02adee] cursor-pointer"
+            class="text-[#0B294F] mt-5 text-center md:text-2xl font-bold hover:text-[#02adee] cursor-pointer"
           >
             Outdoor Retreat with Separate Public Entry
           </h3>
       </li>
 
-      <li class="3xs:mt-32 md:mt-0">
+      <li class="3xs:mt-7 lg:mt-0">
         <div
           class="features-card"
           style="background-image: url('/security.jpg'); background-repeat: no-repeat; background-size: cover;"
@@ -57,13 +57,13 @@
           </div>
         </div>
         <h3
-            class="text-[#0B294F] mt-5 text-center text-2xl font-bold hover:text-[#02adee] cursor-pointer"
+            class="text-[#0B294F] mt-5 text-center md:text-2xl font-bold hover:text-[#02adee] cursor-pointer"
           >
             All Round Compound wall with 24/7 Security
           </h3>
       </li>
 
-      <li class="3xs:mt-32 md:mt-0 md:mt-16 lg:mt-0 3xs:mt-32 ">
+      <li class="3xs:mt-7 md:mt-16 lg:mt-0">
         <div
           class="features-card"
           style="background-image: url('/rainwater.jpg'); background-repeat: no-repeat; background-size: cover;"
@@ -75,13 +75,13 @@
           </div>
         </div>
         <h3
-            class="text-[#0B294F] mt-5 text-center text-2xl font-bold hover:text-[#02adee] cursor-pointer"
+            class="text-[#0B294F] mt-5 text-center md:text-2xl font-bold hover:text-[#02adee] cursor-pointer"
           >
             Rain Water Harvesting
           </h3>
       </li>
 
-      <li class="md:mt-16 3xs:mt-32">
+      <li class="md:mt-16 ">
         <div
           class="features-card"
           style="background-image: url('/led.jpg'); background-repeat: no-repeat; background-size: cover;"
@@ -93,13 +93,13 @@
           </div>
         </div>
         <h3
-            class="text-[#0B294F] mt-5 text-center text-2xl font-bold hover:text-[#02adee] cursor-pointer"
+            class="text-[#0B294F] mt-5 text-center md:text-2xl font-bold hover:text-[#02adee] cursor-pointer"
           >
             LED Street Lights
           </h3>
       </li>
 
-      <li class="md:mt-16 3xs:mt-32">
+      <li class="md:mt-16 ">
         <div
           class="features-card"
           style="background-image: url('/drinageline.jpg'); background-repeat: no-repeat; background-size: cover;"
@@ -111,13 +111,13 @@
           </div>
         </div>
         <h3
-            class="text-[#0B294F] mt-5 text-center text-2xl font-bold hover:text-[#02adee] cursor-pointer"
+            class="text-[#0B294F] mt-5 text-center md:text-2xl font-bold hover:text-[#02adee] cursor-pointer"
           >
             Drinage Line
           </h3>
       </li>
 
-      <li class="md:mt-16 3xs:mt-32">
+      <li class="md:mt-16 ">
         <div
           class="features-card"
           style="background-image: url('/arch.jpg'); background-repeat: no-repeat; background-size: cover;"
@@ -133,13 +133,13 @@
           </div>
         </div>
         <h3
-            class="text-[#0B294F] mt-5 text-center text-2xl font-bold hover:text-[#02adee] cursor-pointer"
+            class="text-[#0B294F] mt-5 text-center md:text-2xl font-bold hover:text-[#02adee] cursor-pointer"
           >
             Internal 40 feet road connectivity
           </h3>
       </li>
 
-      <li class="md:mt-16 3xs:mt-32">
+      <li class="md:mt-16 ">
         <div
           class="features-card"
           style="background-image: url('/water.jpg'); background-repeat: no-repeat; background-size: cover;"
@@ -151,12 +151,12 @@
           </div>
         </div>
         <h3
-            class="text-[#0B294F] mt-5 text-center text-2xl font-bold hover:text-[#02adee] cursor-pointer"
+            class="text-[#0B294F] mt-5 text-center md:text-2xl font-bold hover:text-[#02adee] cursor-pointer"
           >
             Drinking Water
           </h3>
       </li>
-      <li class="md:mt-16 3xs:mt-32">
+      <li class="md:mt-16 ">
         <div
           class="features-card h-[20vh]"
           style="background-image: url('/electiricty.jpg'); background-repeat: no-repeat; background-size: cover;"
@@ -172,12 +172,12 @@
           </div>
         </div>
         <h3
-            class="text-[#0B294F] mt-5 text-center text-2xl font-bold hover:text-[#02adee] cursor-pointer"
+            class="text-[#0B294F] mt-5 text-center md:text-2xl font-bold hover:text-[#02adee] cursor-pointer"
           >
             Electricity
           </h3>
       </li>
-      <li class="md:mt-16 3xs:mt-32">
+      <li class="md:mt-16 ">
         <div
           class="features-card"
           style="background-image: url('/park.jpg'); background-repeat: no-repeat; background-size: cover;"
@@ -189,7 +189,7 @@
           </div>
         </div>
         <h3
-            class="text-[#0B294F] mt-5 text-center text-2xl font-bold hover:text-[#02adee] cursor-pointer"
+            class="text-[#0B294F] mt-5 text-center md:text-2xl font-bold hover:text-[#02adee] cursor-pointer"
           >
             Park
           </h3>
@@ -197,3 +197,6 @@
     </ul>
   </div>
 </section>
+
+
+
